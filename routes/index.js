c97da0cb2a8adba8c015b6e1783bae14b6482165
Ryan Wolstenholme <ryanwolstenholme@gmail.com>
2015-04-19 10:58:10 +1200

    Add search page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/search', function(req, res) {
+  if (!req.query) return res.sendStatus(400);
+  res.render('searchres', { search: req.query.inp });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'unicode-webapp' });
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var should = require('chai').should();
+var expect = require('chai').expect;
 var request = require('supertest')(require('../app.js'));
 
 describe('index page', function() {
@@ -14,5 +14,23 @@ describe('favicon', function() {
         request
             .get('/favicon.ico')
             .expect(200, done);
+    });
+});
+
+describe('search page', function() {
+    it('should return 200 status', function(done) {
+        request
+            .get('/search?inp=asdfas')
+            .expect(200, done)
+    });
+
+    it('should contain search string', function(done) {
+        request
+            .get('/search?inp=asdfas')
+            .expect(hasSearchTerm)
+            .end(done);
+        function hasSearchTerm(res){
+            expect(res.text.indexOf('asdfas')).not.equal(-1);
+        }
     });
 });
