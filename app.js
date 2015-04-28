835bb3d641bbf7a8edc1f0c5307b5a2e0b352350
Ryan Wolstenholme <ryanwolstenholme@gmail.com>
2015-04-29 07:23:55 +1200

    Show status code in prod environment

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
+    message: err.status + " " + err.message,
     error: {}
   });
 });
