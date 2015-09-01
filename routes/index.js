092fdd8d08c2220aa27bd02d1ca6a61989ab2b5d
Ryan Wolstenholme <ryanwolstenholme@gmail.com>
2015-09-02 05:51:14 +1200

    Spelling/Consistency changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'unicode-webapp' });
+    res.render('index', { title: 'Unicode-Website' });
 });
 
 module.exports = router;
